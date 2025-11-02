1608e2d38737a0f19fe690732327dcb777deb21a
Prakashraj2404 <prakashmadhaiyan02@gmail.com>
2025-11-02 23:47:43 +0530

    Initial Push

---
+++ src/Constants/uiconstants.ts
@@ -0,0 +1,33 @@
+export const COLORS = {
+    primary_red: '#ED1C24',
+    primary_yellow: '#FFDD00',
+    primary_gray: '#707070',
+    primary_black: '#000000',
+    primary_white: '#000000'
+};
+
+// uiConstants.js
+
+export const FONTS = {
+  regular: {
+    fontFamily: '"Source Serif 4", serif',
+    fontOpticalSizing: 'auto',
+    fontWeight: 400,
+    fontStyle: 'normal',
+  },
+  medium: {
+    fontFamily: '"Source Serif 4", serif',
+    fontOpticalSizing: 'auto',
+    fontWeight: 500,
+    fontStyle: 'normal',
+  },
+  boldHeading: {
+    fontFamily: '"Source Serif 4", serif',
+    fontOpticalSizing: 'auto',
+    fontWeight: 600,
+    fontStyle: 'normal',
+    fontSize: '30px',
+  },
+};
+
+
