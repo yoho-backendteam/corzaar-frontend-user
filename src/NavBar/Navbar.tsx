43bfeac2ff98991614909f3515e8fcda868542c1
SelshiyaMaryYoho <selshiyamary.yoho@gmail.com>
2025-11-04 17:12:29 +0530

    changes in

--- src/NavBar/Navbar.tsx
+++ src/NavBar/Navbar.tsx
@@ -1,20 +1,28 @@
 import React, { useState } from 'react'
-import { FiMenu, FiX, FiSearch, FiMapPin ,FiChevronDown } from "react-icons/fi";
+import { FiMenu, FiX, FiSearch, FiMapPin ,FiChevronDown} from "react-icons/fi";
+import { FiUser } from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
+import { IoSettingsOutline } from "react-icons/io5";
 import { Link, NavLink } from 'react-router-dom';
 import logocap from '../assets/images/logocap.png'
 import { COLORS, FONTS } from '../Constants/uiconstants';
 import carticon from '../assets/images/shopping-cart.png'
 import notify from '../assets/images/notification.png'
 import profileimg from '../assets/images/profileimg.png'
+import logout from '../assets/images/export.svg'
+
 
 const Navbar:React.FC = () => {
     const [menuOpen, setMenuOpen] = useState(false);
     const [showDropdown, setShowDropdown] = useState(false);
     const [location, setLocation] = useState("Mumbai, India");
-    const locations = ["Mumbai, India", "Delhi, India", "Bangalore, India", "Chennai, India", "Pune, India"];
+     const locations = ["Chennai", "Bangalore", "Pune", "Salem"];
+
+     const [showProfile, setShowProfile] = useState(false);
   return (
     <div>
-          <nav className="shadow-md sticky top-0 z-50" style={{ ...(FONTS.regular as any) , background : COLORS.primary_white}}>
+          
+    <nav className="shadow-md sticky top-0 z-50" style={{ ...(FONTS.regular as any) , background : COLORS.primary_white}}>
       <div className="max-w-7xl mx-auto flex items-center justify-between p-7 gap-4">
 
        <NavLink to="/">
@@ -71,48 +79,45 @@ const Navbar:React.FC = () => {
     </div>
 
       
-        <div className="hidden md:flex items-center gap-4">
-        
-                    <div className="relative">
-            <button
-              onClick={() => setShowDropdown(!showDropdown)}
-              className="flex items-center p-2 rounded"
-              style={{ color: COLORS.primary_gray, background: "#FFFBD3" }}
-            >
-              <FiMapPin className="mr-1" />
-              <span>{location}</span>
-              <FiChevronDown className="ml-1" />
-            </button>
-            {showDropdown && (
+        <div className="hidden md:flex items-center gap-4"> 
+<div className="relative">
+      <button
+        onClick={() => setShowDropdown(!showDropdown)}
+        className="flex items-center px-3 py-2 rounded-md text-sm font-medium"
+        style={{ color: COLORS.primary_gray, background: '#FFFBD3' }}
+      >
+        <FiMapPin className="mr-1 text-red-500" />
+        <span>{location}</span>
+        <FiChevronDown className="ml-1" />
+      </button> 
+      {showDropdown && (
+        <div
+          className="absolute mt-2 w-40 rounded-xl shadow-lg p-3 border z-50"
+          style={{background: '#FFFBD3'}}
+        >
+          <div className="space-y-2">
+            {locations.map((loc) => (
               <div
-                className="absolute mt-2 w-44 rounded-lg shadow-md border z-50"
-                style={{ background: COLORS.primary_white }}
+                key={loc}
+                onClick={() => {
+                  setLocation(loc);
+                  setShowDropdown(false);
+                }}
+                className="flex items-center gap-2 bg-white px-3 py-2 rounded-md cursor-pointer hover:bg-gray-50 text-red-500 font-medium"
               >
-                {locations.map((loc) => (
-                  <div
-                    key={loc}
-                    onClick={() => {
-                      setLocation(loc);
-                      setShowDropdown(false);
-                    }}
-                    className="px-4 py-2 hover:bg-gray-100 cursor-pointer text-sm"
-                  >
-                    {loc}
-                  </div>
-                ))}
+                <FiMapPin /> {loc}
               </div>
-            )}
+            ))}
           </div>
-
-
-
-          
+        </div>
+      )}
+    </div>  
           <div className="flex items-center p-2 rounded" style={{background: '#FFFBD3'}}>
-            <FiSearch className="text-gray-600 mr-2" />
+            <FiSearch className="mr-2" style={{color:COLORS.primary_gray}}/>
             <input
               type="text"
               placeholder="Search..."
-              className=" outline-none w-40"
+              className=" w-40"
               style={{color:COLORS.primary_gray}}
             />
           </div>
@@ -123,10 +128,41 @@ const Navbar:React.FC = () => {
         <Link to="/notificationPage">
         <img src={notify} className="rounded-full p-2 cursor-pointer" style={{ background : COLORS.primary_red}}/>
          </Link>  
-         <Link to="/profilePage">
-          <img src={profileimg} alt="User" className="w-10 h-10 rounded-full object-cover" />
-                
-         </Link>
+       
+        <div className="relative"> 
+          <img
+        src={profileimg}
+        alt="User"
+        className="w-10 h-10 rounded-full object-cover cursor-pointer"
+        onClick={() => setShowProfile((prev) => !prev)}
+      />
+
+
+      {showProfile && (
+           <div className="absolute right-0 mt-3 w-64 bg-white shadow-lg rounded-2xl p-5 z-50">
+          <h2 className="text-lg font-semibold text-gray-900">Rahul Sharma</h2>
+          <p className="text-gray-500 text-sm mb-4">Student</p>
+
+          <ul className="space-y-3 text-gray-700">
+            <li className="flex items-center gap-2 hover:text-black cursor-pointer">
+              <FiUser size={16} /> My Profile
+              
+            </li>
+            <li className="flex items-center gap-2 hover:text-black cursor-pointer">
+              <FiShoppingCart size={16} /> My Courses
+            </li>
+            <li className="flex items-center gap-2 hover:text-black cursor-pointer">
+              <IoSettingsOutline size={16} /> Settings
+            </li>
+          </ul>
+
+          <button className="flex items-center justify-center gap-1  w-full py-2 mt-5 rounded-md hover:bg-red-600 transition" style={{background : COLORS.primary_red , color:COLORS.primary_white}}>
+           <img src={logout} alt="logout icon" className="w-5 h-5" />
+         Logout
+          </button>
+        </div>
+      )}
+    </div>
         </div>
 
        
